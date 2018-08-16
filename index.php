4dc0440ce675026efdbf9edf7039383c18a4374e
ZM05 <41561590+ZM05@users.noreply.github.com>
2018-08-15 19:43:56 -0500

    Meta's

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta charset="UTF-8">
-	<title>Login</title>
+	<meta name="description" content="Un lugar donde puedes comprar libros a muy buen precio">
+	<meta name="keywords" content="biblioteca,compra de libros, libros, comprar, leer">
+	<meta name="author" content="Diego Saravia">
+	<title>Login | La biblioteca del mundo</title>
 </head>
 <body>
 	
