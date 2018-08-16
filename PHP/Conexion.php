881a8599259fc52d3fb023641902976062538d8e
ZM05 <41561590+ZM05@users.noreply.github.com>
2018-08-15 20:16:33 -0500

    arrayOptions

--- PHP/Conexion.php
+++ PHP/Conexion.php
@@ -4,11 +4,12 @@
 	{
 		function __construct()
 		{
-			$conexion = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_BBDD,DB_USER,DB_PASSWORD);
+			$conexion = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_BBDD,DB_USER,DB_PASSWORD,arrayOptions);
 		}
 
 		public function insertQuery()
 		{
+
 
 		}
 
@@ -32,5 +33,11 @@
 		private DB_USER = "root";
 		private DB_PASSWORD = "";
 		private $conexion;
+		private $arrayOptions = array(
+			PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_EMULATE_PREPARES=>FALSE,
+			PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'",
+			PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ
+		);
 	}
  ?>
