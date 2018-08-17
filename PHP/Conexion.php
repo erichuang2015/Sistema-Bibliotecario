dfeeb6d2803793d53acee8f1715aa1fcbdd8702d
ZM05 <41561590+ZM05@users.noreply.github.com>
2018-08-17 06:29:37 -0500

    InsertQuery V1
    Terminando de hacer el método InsertQuery de la clase Conexión

--- PHP/Conexion.php
+++ PHP/Conexion.php
@@ -4,30 +4,57 @@
 	{
 		function __construct()
 		{
-			$conexion = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_BBDD,DB_USER,DB_PASSWORD,arrayOptions);
+			try {
+				
+				$conexion = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_BBDD,DB_USER,DB_PASSWORD,arrayOptions);
+				exit();
+			} catch (PDOException $e) {
+				
+				die("Error de conexión " . $e->getMessage() . " en la línea " . $e->getLine());
+			}
 		}
 
-		public function insertQuery()
+		//Métodos de consulta que devuelven arrays
+		public function InsertQuery(string $query, array $marcadores)
 		{
+			$parametros = func_get_args();
 
+			try {
+				if (count($parametros) == 1) {
+				
+					$cons_prep = $this->conexion->prepare(query);
+					$cons_prep->execute();
+				} elseif (count($parametros) == 2) {
 
+					$cons_prep = $this->conexion->prepare(query);
+					$cons_prep->execute($parametros[1]);
+				} else {
+				
+					throw new ParamsException("Error de paso de parámetros");
+							
+				}
+			} catch (ParamsException $e) {
+				
+				echo $e->getMessage();
+			}
 		}
 
-		public function deleteQuery()
+		public function deleteQuery(string $query)
 		{
 			# code...
 		}
 
-		public function updateQuery()
+		public function updateQuery(string query)
 		{
 
 		}
 
-		public function selectQuery()
+		public function selectQuery(string query)
 		{
 			# code...
 		}
-	
+		
+		// Variables usadas para establecer conexión y las configuraciones pertinentes
 		private DB_SERVER = "localhost";
 		private DB_BBDD = "";
 		private DB_USER = "root";
