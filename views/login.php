1cbfce000d3a41b82bce322c39a851917c17048e
Diego (Computo) <41561590+ZM05@users.noreply.github.com>
2018-08-29 14:37:17 -0600

    Login
    Se ha agregado la vista del login

---
+++ views/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	
+	<div class="login">
+		<form action="" method="post" class="login2"  >
+			<h1 style="color: #ffffff; font-family: 'Love Ya Like A Sister', cursive;text-shadow: 2px 2px 4px #000000; font-size: 40px;">Iniciar Sesión</h1>
+			<br>
+			<label for="Usuario"><p><b>Nombre de Usuario</b></p>
+			<p><input type= "text" name= usuario placeholder= "Usuario" id="Usuario" require></p>
+			</label>
+			<br><br>
+			<label for="Contraseña"> <p><b>Contraseña</b></p>
+			<input type="password" name=contraseña placeholder="Contraseña" id="Contraseña" require>
+			</label>
+			<br><br>
+			<input type="submit" value="Iniciar sesión" name="" >
+		
+		</form>
+	</div>	
+	
+</body>
+</html>
