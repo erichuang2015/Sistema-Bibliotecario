2b14bb3abf6c2b1f981a7b5d13a09a373b8fb32e
Diego (Computo) <41561590+ZM05@users.noreply.github.com>
2018-09-05 15:39:09 -0600

    Puebas para insertar un nuevo usuario
    Empecé las pruebas pero no las terminé. El error está en que no se están insertando datos del mismo tipo de datos de las columnas.

--- controllers/registroController.php
+++ controllers/registroController.php
@@ -23,9 +23,18 @@
 		
 		$valores = [":nombres, :apellidos, :apodo, :contra, :email, :nacimiento, :edad, :sexo"];
 
-		$marcadores = [":nombres" => $nombres, ":apellidos" => $apellidos, ":apodo" => $nom_usuario, ":contra" => $p_cifrada, ":email" =>  $email, ":nacimiento" => $born, ":edad" => $edad, ":sexo" => $sexo];
+		$marcadores = array(":nombres" => $nombres, ":apellidos" => $apellidos, ":apodo" => $nom_usuario, ":contra" => $p_cifrada, ":email" =>  $email, ":nacimiento" => $born, ":edad" => $edad, ":sexo" => $sexo);
 
-		$conexion->InsertQuery("usuarios", $valores, null, $marcadores);
+		$arrayOptions = array(
+			PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_EMULATE_PREPARES=>FALSE,
+			PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'",
+			PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC
+		);
+
+		$conexion = new PDO("mysql:host=localhost;dbname=usuarios","root","", $arrayOptions);
+
+		$conexion->prepare($query);
 
 		header("location:../views/login.php");
 
