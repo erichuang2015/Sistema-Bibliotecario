31c0cd1f99ffe6ab3998dc71b6ed9baa81bdefdf
ZM05 <41561590+ZM05@users.noreply.github.com>
2018-09-07 02:10:05 -0600

    inicio1
    Tomar en cuenta Inicio1

---
+++ views/Inicio1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema Bibliotecario | Inicio</title>
+	<link rel="stylesheet" type="text/css" href="../views/css/Inicio1.css">
+	<link rel="stylesheet" type="text/css" href="../views/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../views/css/fuentes.css">
+</head>
+<body>
+	<div class="nav container-">
+		<ul>
+			<li><a href=""></a>Acerca de nosotros</li>
+			<li><a href=""></a>Inicio</li>
+		</ul>
+	</div>
+	
+
+
+
+
+
+	<script src="../views/js/jquery.js"></script>
+	<script src="../views/js/bootstrap.min.js"></script>
+</body>
+</html>
